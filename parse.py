0991a8efdaed533f5f8d2f5e4565762d7b92d229
1tanguysimon1-maker <1tanguysimon1@gmail.com>
2026-06-29 13:03:16 +0200

    Refactor time zone handling for Paris departures

--- parse.py
+++ parse.py
@@ -1,11 +1,8 @@
 import json, sys
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
 
-def to_paris(dt):
-    # Décalage Europe/Paris : UTC+1 hiver, UTC+2 été (approximation DST)
-    import time
-    offset = 2 if time.daylight and time.localtime().tm_isdst else 1
-    return dt + timedelta(hours=offset)
+PARIS = ZoneInfo("Europe/Paris")
 
 data = json.load(sys.stdin)
 visits = data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit']
@@ -14,7 +11,7 @@ out = {'updated_at': now.strftime('%Y-%m-%dT%H:%M:%SZ'), 'departures': []}
 for v in visits[:4]:
     dt_str = v['MonitoredVehicleJourney']['MonitoredCall']['ExpectedDepartureTime']
     dt = datetime.fromisoformat(dt_str.replace('Z', '+00:00'))
-    dt_paris = to_paris(dt)
+    dt_paris = dt.astimezone(PARIS)
     out['departures'].append({
         'time': dt_paris.strftime('%H:%M'),
         'minutes': max(0, int((dt - now).total_seconds() // 60))
